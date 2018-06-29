08215bfb273dca2b14db47eb564b7cfaef096a6b
Gaspar Ricci <gasparricci.p95@gmail.com>
2018-06-29 17:21:16 -0400

    Added the lexer and tokenizer. The lexer defines the rules for valid SECD tokens. The tokenizer uses this lexer to tokenize the source code. The lexing rules: * Ignore whitespace and line-breaks, as they have no meaning in the program. * Define the reserved terms of the language. Instruction identifiers (INT_CONST, ADD, SUB), integer parameters and the parentheses "(" and ")".

--- main.rkt
+++
@@ -1,7 +0,0 @@
-#lang br
-
-; When someone uses the secd language by writing #lang secd, Racket looks for a reader module
-; in the secd package and requires a read-syntax function from it.
-(module reader br
-  (require "reader.rkt")
-  (provide read-syntax))
---
+++ test.rkt
@@ -0,0 +1,2 @@
+#lang secd/tokenize-only
+((INT_CONST 45) (INT_CONST 837) (ADD))
