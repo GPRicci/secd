516a45d525228e7008032b3dcec4d04345e1326a
Gaspar Ricci <gasparricci.p95@gmail.com>
2018-06-29 18:27:30 -0400

    The reader is finished. The SECD language now provides a reader module with a read-syntax function. Since the expander has not been defined yet, we are temporarily using the expander for the br language.

---
+++ main.rkt
@@ -0,0 +1,16 @@
+#lang br/quicklang
+(require "tokenizer.rkt" "parser.rkt")
+
+; Every language needs to provide a reader module from which racket can get the read-syntax function.
+; Once found, racket call read-syntax passing the path to the source file and a port from which the function can read
+; the contents.
+; read-syntax returns a syntax object with a single module containing the parse tree and declaring the expander
+; which will provide the necessary bindings to evaluate the parse tree as if it were a Racket program.
+(define (read-syntax path port)
+  (define parse-tree (parse path (make-tokenizer port path)))
+  (strip-bindings
+  #`(module secd-mod br ; Since we haven't yet defined our own expander, we'll temporarily use the expander of the br language. This should become secd/expander.
+      #,parse-tree))
+  )
+
+(module+ reader (provide read-syntax))
